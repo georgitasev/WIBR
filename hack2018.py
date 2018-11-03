528de8a179fb9d25c1823f312aeb6ac88846a439
Verella, Jean Tipan <tipan.verella@kohls.com>
2018-11-03 14:49:18 -0500

    init somehow

---
+++ hack2018.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import usadress
+
+s = pd.Series({1:"District 1: Ashanti Hamilton",
+2:"District 2: Cavalier Johnson",
+3:"District 3: Nicholas Kovac",
+4:"District 4: Robert Bauman",
+5:"District 5: James Bohl, Jr.",
+6:"District 6: Milele A. Coggs",
+7:"District 7: Khalif J. Rainey",
+8:"District 8: Robert G. Donovan",
+9:"District 9: Chantia Lewis",
+10:"District 10: Michael J. Murphy",
+11:"District 11: Mark A. Borkowski",
+12:"District 12: José G. Pérez",
+13:"District 13: Terry L. Witkowski",
+14:"District 14: T. Anthony Zielinski",
+15:"District 15: Russell W. Stamper, II"})
